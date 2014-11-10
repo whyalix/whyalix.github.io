244e95836bc2eeded535ca0b7cbfd0477e27da4a
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-11-10 12:51:23 +0100

    \#15: refactored and improved JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,120 @@
-$(function(){
+(function ($, global) {
 
-  var $container = $('.gallery');
-  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+  var App = function ( conf ) {
+    this.conf = $.extend({
+      container: '.gallery',
+      lazyload: true,
 
-  $container.isotope({
-    itemSelector : '.gallery__item',
-    masonry : {
-      columnWidth : 410
+      // Extra options
+      init: true
+    }, conf || {});
+
+    this.width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+    this.ratio = window.devicePixelRatio;
+
+    if (this.conf.init !== false) {
+      this.initialize();
     }
-  });
+  };
 
-  $('.filters a').click(function(){
-    var selector = $(this).attr('data-filter');
-    $container.isotope({ filter: selector });
-    return false;
-  });
+  App.prototype.initialize = function () {
+    this.isotope();
 
-  if(width > 767) {
-    $('.gallery__item').on('click', function() {
-      var $requested = $(this).find('.gallery__image');
-      openDialog($requested.attr('src'), $requested.attr('alt'));
+    if (this.conf.lazyload) {
+      this.lazyload();
+    }
+
+    if (this.width > 767) {
+      this.avgrund.initialize(this);
+    }
+
+    if (this.ratio > 1) {
+      this.retina.initialize();
+    }
+  };
+
+  App.prototype.isotope = function () {
+    var $container = $(this.conf.container);
+
+    $container.isotope({
+      itemSelector: '.gallery__item',
+      masonry: {
+        columnWidth: 410
+      }
     });
-  }
 
-  $('.gallery__image').lazyload({
-    effect: 'fadeIn',
-    threshold: 200,
-    failure_limit : 10
-  })
+    $('.filters > li').on('click', function (event) {
+      $container.isotope({
+        filter: $(this).attr('data-filter')
+      });
+    });
+  };
 
-});
+  App.prototype.avgrund = {};
 
-function openDialog(src, alt) {
-  var fragments = src.split('.'),
-    path = (fragments[0].slice(-3) == "@2x"
-        ? fragments[0] +    '.' + fragments[1]
-        : fragments[0] + '@2x.' + fragments[1]);
-  var $image = $('.avgrund-popup__image');
+  App.prototype.avgrund.initialize = function ( context ) {
+    $('.gallery__item').on('click', $.proxy(function (event) {
+      var $requested = $(event.target);
+      this.open(context.retina.check2x($requested.attr('src')), $requested.attr('alt'));
+    }, this));
 
-  $image.attr({
-    'src': path,
-    'alt':alt
-  });
+    $('.avgrund-popup__close').on('click', $.proxy(function () {
+      this.close();
+    }, this));
+  };
 
-  Avgrund.show('.avgrund-popup');
-}
+  App.prototype.avgrund.open = function ( src, alt ) {
+    $('.avgrund-popup__image').attr({
+      'src': src,
+      'alt': alt
+    });
 
-function closeDialog() {
-  Avgrund.hide();
+    global.Avgrund.show('.avgrund-popup');
+  };
 
-  $('.avgrund-popup__image').attr({
-    'src': 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
-    'alt': ''
-  });
-}
+  App.prototype.avgrund.close = function () {
+    global.Avgrund.hide();
 
-function check2x(path) {
-  var fileName = path.substr(0, path.lastIndexOf('.'));
-  return fileName.substr(-3) !== "@2x"
-       ? fileName + "@2x" + path.substr(path.lastIndexOf('.'))
-       : path ;
-}
+    $('.avgrund-popup__image').attr({
+      'src': 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+      'alt': ''
+    });
+  };
 
-var retina = window.devicePixelRatio > 1;
+  App.prototype.lazyload = function () {
+    $('.gallery__image').lazyload({
+      effect: 'fadeIn',
+      threshold: 200,
+      failure_limit: 10
+    });
+  };
 
-if (retina) {
-  var img = document.getElementsByTagName('img');
+  App.prototype.retina = {};
 
-  for(var i = 0, len = img.length; i < len; i++) {
+  App.prototype.retina.initialize = function () {
+    $('.gallery__image').each($.proxy(function (index, element) {
+      var $current = $(element);
+      var src = $current.attr('src');
+      var original = $current.attr('data-original');
 
-    var current  = img[i],
-        src      = current.getAttribute('src'),
-        original = current.getAttribute('data-original');
+      if (src.substr(0, 4) !== 'data') {
+        $current.attr('src', this.check2x(src));
+      }
 
-    if (src.substr(0, 4) !== 'data') {
-      current.setAttribute('src', check2x(src));
-    }
+      else if (original !== null && original.substr(0, 4) !== 'data') {
+        $current.attr('data-original', this.check2x(original));
+      }
+    }, this));
+  };
 
-    else if (original !== null && original.substr(0, 4) !== 'data') {
-      current.setAttribute('data-original', check2x(original));
-    }
-  }
-}
+  App.prototype.retina.check2x = function ( path ) {
+    var fileName = path.substr(0, path.lastIndexOf('.'));
+
+    return fileName.substr(-3) !== "@2x"
+         ? fileName + "@2x" + path.substr(path.lastIndexOf('.'))
+         : path ;
+  };
+
+  global.App = App;
+
+}(window.jQuery, window));
